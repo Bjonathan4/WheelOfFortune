60b04860d6f47b559b6e7817578408e01b8d1cf4
Lennon05 <lennonlavarier@gmail.com>
2024-04-05 12:44:15 -0500

    Adding in the Solver function, with extra code in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scan = new Scanner(System.in);
+
     // Wheel Spin Function
     public static int[] randomArray(int size) {
         Random random = new Random();
@@ -10,8 +13,37 @@ public class Main {
         }
         return a;
     }
-    public static void main(String[] args) {
+    public static int solver(String fullPhrase, String partialPhrase, int guesses){
+        System.out.println(partialPhrase);
+        System.out.println("Please enter your guess for full phrase:");
+        String choice = scan.nextLine();
+        if (choice.equalsIgnoreCase(fullPhrase)) {
+            return 1;
+        } else if (guesses > 0) {
+            return 2;
+        } else {
+            return 3;
+        }
 
-        System.out.println("Hello world!");
+
+    }
+    public static void main(String[] args) {
+        //activates functions for category and phrase
+        System.out.println("Welcome to wheel of fortune!");
+        int guesses = 2;
+        boolean flag = true;
+        while (flag) {
+            int winState = solver("oh yeah", "oh ____", guesses);
+            if (winState == 1) {
+                System.out.println("You Win!");
+                flag = false;
+            } else if (winState == 2) {
+                System.out.println("Incorrect, Try Again!");
+                guesses = guesses - 1;
+            } else if (winState == 3) {
+                System.out.println("You Lose!");
+                flag = false;
+            }
+        }
     }
 }
