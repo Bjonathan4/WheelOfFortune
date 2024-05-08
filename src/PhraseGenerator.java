60cfcf615e9ba882ba4c53f19da6dae02fd90596
jonathanburorabravo <bburora@millikin.edu>
2024-05-08 14:03:59 -0500

    added a class on my code.

--- src/PhraseGenerator.java
+++ src/PhraseGenerator.java
@@ -1,20 +1,26 @@
 import java.util.*;
 
 public class PhraseGenerator {
+    // Define a map to store categories (e.g., Movie, Song, Country) and corresponding phrases
     private static final Map<String, List<String>> categoriesAndPhrases = new HashMap<>();
-
+    // Fill out the map with specified groups and the related phrases.
     static {
-        categoriesAndPhrases.put("Movie", Arrays.asList("The Godfather", "The Shawshank Redemption", "Forrest Gump"));
-        categoriesAndPhrases.put("Song", Arrays.asList("Bohemian Rhapsody", "Imagine", "Hey Jude"));
-        categoriesAndPhrases.put("Country", Arrays.asList("United States", "Canada", "Australia"));
+        categoriesAndPhrases.put("Movie", Arrays.asList("The Godfather", "The Shawshank Redemption", "Hotel Rwanda","The family plan"));
+        categoriesAndPhrases.put("Song", Arrays.asList("like that", "Hot line bling", "Hello","Not like us "));
+        categoriesAndPhrases.put("Country", Arrays.asList("United States", "Canada", "Rwanda","Kenya"));
     }
-
+    //Method for creating a random category and phrase pair
     public String[] generateCategoryAndPhrase() {
         Random random = new Random();
+        // Get all categories from the map
         List<String> categories = new ArrayList<>(categoriesAndPhrases.keySet());
+        //Select a random category from the list of categories
         String randomCategory = categories.get(random.nextInt(categories.size()));
+        //get the list of phrases associated with the randomly selected category
         List<String> phrases = categoriesAndPhrases.get(randomCategory);
+        //Select a random phrase from the list of phrases associated with the selected category
         String randomPhrase = phrases.get(random.nextInt(phrases.size()));
+        //Return the random category and phrase as an array
         return new String[]{randomCategory, randomPhrase};
     }
 
